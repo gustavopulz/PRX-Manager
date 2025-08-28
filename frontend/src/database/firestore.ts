24c56c2dfd6ffd040265d9abb4b9c0fa76960572
GRoxo-B <gui.basso@gmail.com>
2025-08-28 12:11:49 -0300

    fix: remove unnecessary comment regarding measurementId in firestore.ts

--- frontend/src/database/firestore.ts
+++ frontend/src/database/firestore.ts
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
